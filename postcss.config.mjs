77dffb39a5413dc16affd6ddcf14197d8230b57b
JoeDevth <chaiwatbutworn@gmail.com>
2025-03-14 13:57:32 +0700

    fix tsconfig

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,40 +1,9 @@
-// Import the necessary utilities using import
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    // your paths
-    "./src/**/*.{ts,tsx}",
+  plugins: [
+    tailwindcss(),
+    autoprefixer(),
   ],
-  darkMode: "class",
-  theme: {
-    extend: {
-      animation: {
-        aurora: "aurora 60s linear infinite",
-      },
-      keyframes: {
-        aurora: {
-          from: {
-            backgroundPosition: "50% 50%, 50% 50%",
-          },
-          to: {
-            backgroundPosition: "350% 50%, 350% 50%",
-          },
-        },
-      },
-    },
-  },
-  plugins: [addVariablesForColors],
 };
-
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
-
-  addBase({
-    ":root": newVars,
-  });
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,22 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
 import "./globals.css";
 import { GameProvider } from "@/context/GameContext";
 import { AuroraBackground } from "@/components/ui/aurora-background";
 
-const sansserif = Poppins({
-  weight: ["100", "300", "200", "400", "700", "900"],
+import { Geist, Geist_Mono } from "next/font/google";
+
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
   subsets: ["latin"],
 });
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
 
 export const metadata: Metadata = {
   title: "card game counting",
@@ -21,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={` ${sansserif.className}  antialiased`}>
+      <body   className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
         <GameProvider>
           <AuroraBackground>
             {children}
